e386c828a2579f154abb0a65218c7b85fc994180
Leslie Jobse <leslie@jobse.be>
2019-08-10 18:56:20 +0200

    remove build

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import TranslatorParam from './translatorParam';
-import Translator from './translator';
-declare const _default: {
-    TranslatorParam: typeof TranslatorParam;
-    Translator: typeof Translator;
-    Initialize: typeof Translator.initialize;
-};
-export = _default;
--- lib/translatorParam.d.ts
+++
@@ -1,6 +0,0 @@
-declare class TranslatorParam {
-    key: string;
-    value: string;
-    constructor(key: string, value: string);
-}
-export default TranslatorParam;
